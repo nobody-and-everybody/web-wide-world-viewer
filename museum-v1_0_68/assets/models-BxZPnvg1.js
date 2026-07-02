616cbcfc5dc06e6608a6910fb9e55f096cde592f
vincentzsun <vincentzsun@tencent.com>
2026-07-02 11:17:15 +0800

    增加文件

---
+++ museum-v1_0_68/assets/models-BxZPnvg1.js
@@ -0,0 +1,2 @@
+const e={bar:{name:"酒吧场景",des:"一个精美的酒吧场景模型，展示了室内环境的细节"},garden:{name:"花园场景",des:"美丽的花园场景，包含各种植物和景观元素"},hero:{name:"英雄角色",des:"英雄角色模型，高质量的人物建模"},acg:{name:"日本场景",des:"日本场景，包含日本的风土人情"},acg1:{name:"日本场景ksplat",des:"日本场景，包含日本的风土人情"}};export{e as M};
+//# sourceMappingURL=models-BxZPnvg1.js.map
