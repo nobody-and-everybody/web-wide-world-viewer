99e595ba35854f001c3100a800d3bf991209a589
v_djlzhou <v_djlzhou@tencent.com>
2026-08-06 12:45:33 +0800

    publish

--- releases/v1.0.87/assets/scene-api-4W_P4CRQ.js
+++
@@ -1,2 +0,0 @@
-const _="https://api.wuji.qq.com",c="/x/api",s="appid=my_buddy_test&schemaid=scene_pos_config_v2&schemakey=31bfc3054bb54a299ccf2c06a33162d9",t=`/wuji_cache/object?${s}`,I=`${_}${c}${t}`;export{I as W};
-//# sourceMappingURL=scene-api-4W_P4CRQ.js.map
